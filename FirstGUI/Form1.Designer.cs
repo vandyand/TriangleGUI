a9e200d3c0603ff38a30d46d87164e6802a1bbd5
vandyand <vandyand@gmail.com>
2018-08-11 21:05:10 -0400

    first commit of first gui

---
+++ FirstGUI/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace FirstGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArea = new System.Windows.Forms.Button();
+            this.textboxBase = new System.Windows.Forms.TextBox();
+            this.textboxHeight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArea
+            // 
+            this.btnArea.Location = new System.Drawing.Point(17, 222);
+            this.btnArea.Name = "btnArea";
+            this.btnArea.Size = new System.Drawing.Size(100, 32);
+            this.btnArea.TabIndex = 0;
+            this.btnArea.Text = "Calculate Area";
+            this.btnArea.UseVisualStyleBackColor = true;
+            this.btnArea.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textboxBase
+            // 
+            this.textboxBase.Location = new System.Drawing.Point(99, 81);
+            this.textboxBase.Name = "textboxBase";
+            this.textboxBase.Size = new System.Drawing.Size(100, 20);
+            this.textboxBase.TabIndex = 5;
+            this.textboxBase.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // textboxHeight
+            // 
+            this.textboxHeight.Location = new System.Drawing.Point(99, 138);
+            this.textboxHeight.Name = "textboxHeight";
+            this.textboxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textboxHeight.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Height";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArea.Location = new System.Drawing.Point(142, 267);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(100, 16);
+            this.lblArea.TabIndex = 8;
+            this.lblArea.Click += new System.EventHandler(this.lblArea_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Open Sans", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(323, 28);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Calculate Area of Right Triangle";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Base";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(20, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Area";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(142, 222);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 32);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = global::FirstGUI.Properties.Resources._1_qEtFuP79xFlEVKXwGslFwQ;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(629, 410);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textboxHeight);
+            this.Controls.Add(this.textboxBase);
+            this.Controls.Add(this.btnArea);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArea;
+        private System.Windows.Forms.TextBox textboxBase;
+        private System.Windows.Forms.TextBox textboxHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ FirstGUI/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstGUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double baase, height, area;
+            baase = Convert.ToDouble(textboxBase.Text);
+            height = Convert.ToDouble(textboxHeight.Text);
+
+            area = baase * height * 0.5;
+
+            lblArea.Text = area.ToString("n");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblArea_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textboxBase.Text = "";
+            textboxHeight.Text = "";
+            lblArea.Text = "";
+        }
+    }
+}
